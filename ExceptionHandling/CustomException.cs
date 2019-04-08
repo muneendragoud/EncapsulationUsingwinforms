cf3d0e29a611b09dcb3a96abc4ad30dd2ac976f7
muneendra p <muneendragoud@gmail.com>
2019-04-08 18:55:04 +0530

    I have done simpe example on Custom exception handling

---
+++ ExceptionHandling/CustomException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+   public class CustomException:ApplicationException
+    {
+        public void NoneOfZero()
+        {
+            Console.WriteLine("Nothing");
+        }
+        public void DivisionException()
+        {
+            Console.WriteLine("Exception occured, divisor should not be zero");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ExceptionHandling/CustomExceptionHandling.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    public class CustomExceptionHandling
+    {
+        static void Main(string[] args)
+        {
+            int d, div, res;
+            div = Int32.Parse(Console.ReadLine());
+            d = Int32.Parse(Console.ReadLine());
+            try
+            {
+                if (div == 0)
+                {
+                    throw new CustomException();
+                }
+            }
+            catch (CustomException e)
+            {
+                e.DivisionException();
+            }
+
+            res = d / div;
+            Console.WriteLine("Result:{0}", res);
+            Console.ReadLine();
+        }
+    }
+}
