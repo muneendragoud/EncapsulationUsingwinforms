0425f2df44c63a263c877be67818682ae538a9a1
muneendra p <muneendragoud@gmail.com>
2019-04-08 18:29:32 +0530

    ExceptionHandling

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class ExceptionHandling
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int n1, n2;
+                n1 = int.Parse(Console.ReadLine());
+                n2 = int.Parse(Console.ReadLine());
+
+                int result = n1 / n2;
+                Console.WriteLine("Result " + result);
+            }
+            catch(IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(InvalidCastException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine(ex.InnerException);
+            }
+
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            finally
+            {
+                Console.WriteLine("Good");
+            }
+            Console.ReadLine();
+        }
+    }
+}
